4bf6b85c2f972f75896991039d8e4434722075ba
tom maenhout <tom.maenhout@gmail.com>
2021-11-11 17:40:28 -0300

    socket

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//const server = require('http').createServer(app)
+
+
 
 var app = express();
 
